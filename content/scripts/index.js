d5e0008112b656c2c65571e896e409b3798f04a1
Álex Puchades <alex94puchades@gmail.com>
2016-11-08 00:50:23 +0100

    Do not pollute global namespace in javascript files

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -1,20 +1,22 @@
-"use strict";
+(function() {
+	"use strict";
 
-function onMenuToggleClicked(event) {
-	var mainNav = document.getElementsByClassName("nav--main")[0];
-	var pageWrapper = document.getElementsByClassName("page-wrapper")[0];
-	if (!this.classList.contains("btn--pressed")) {
-		mainNav.classList.add("nav--visible");
-		pageWrapper.classList.add("page-wrapper--hidden");
-		this.classList.add("btn--pressed");
-	} else {
-		mainNav.classList.remove("nav--visible");
-		pageWrapper.classList.remove("page-wrapper--hidden");
-		this.classList.remove("btn--pressed");
+	function onMenuToggleClicked(event) {
+		var mainNav = document.getElementById("main-nav");
+		var pageWrapper = document.getElementById("page-wrapper");
+		if (!this.classList.contains("btn--pressed")) {
+			mainNav.classList.add("nav--visible");
+			pageWrapper.classList.add("page-wrapper--hidden");
+			this.classList.add("btn--pressed");
+		} else {
+			mainNav.classList.remove("nav--visible");
+			pageWrapper.classList.remove("page-wrapper--hidden");
+			this.classList.remove("btn--pressed");
+		}
 	}
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-	var menuToggle = document.getElementById("menu-toggle");
-	menuToggle.addEventListener("click", onMenuToggleClicked);
-});
+	document.addEventListener("DOMContentLoaded", function() {
+		var menuToggle = document.getElementById("menu-toggle");
+		menuToggle.addEventListener("click", onMenuToggleClicked);
+	});
+})();
