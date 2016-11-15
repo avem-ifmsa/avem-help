40a320147f926d96eb7f136d5f62070a7b437a74
Álex Puchades <alex94puchades@gmail.com>
2016-11-15 13:44:31 +0100

    Improve article indexing

--- lib/articles.rb
+++ lib/articles.rb
@@ -1,24 +1,61 @@
 module Avem
 	module Helpers
 		module Articles
-			def slug_for(string)
-				string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-			end
-
-			def category_slug(item)
-				slug_for item[:category]
-			end
-
-			def article_slug(item)
-				slug_for item[:title]
-			end
-
 			def article_categories
-				@items.find_all('/articles/*').map {|a| a[:category] }.uniq
+				@items.find_all('/articles/**/*')
+				      .sort_by { |i| category_index(i) }
+				      .map { |i| article_category(i) }
+				      .uniq
 			end
 
 			def category_articles(category)
-				@items.find_all('/articles/*').select {|a| a[:category] == category }
+				@items.find_all('/articles/**/*')
+				      .select { |i| article_category(i) == category }
+				      .sort_by { |i| article_index(i) }
+			end
+
+			def article_category(item)
+				path = item.identifier.without_ext
+				result = %r[^/articles/(?:\d+-)?([^/]+)].match(path)
+				item[:category] || result[1]
+			end
+
+			def category_index(item)
+				path = item.identifier.without_ext
+				result = %r[^/articles/(\d+)-].match(path)
+				item[:category_index] || (result && result[1]) || 0
+			end
+
+			def article_title(item)
+				path = item.identifier.without_ext
+				result = %r[^/articles/[^/]+/(?:\d+-)?([^/]+)].match(path)
+				item[:title] || (result && result[1])
+			end
+
+			def article_index(item)
+				path = item.identifier.without_ext
+				result = %r[^/articles/[^/]+/(\d+)-].match(path)
+				item[:index] || (result && result[1]) || 0
+			end
+
+			def category_link(item)
+				"/#{category_slug item}/"
+			end
+
+			def article_link(item)
+				"/#{category_slug item}/#{article_slug item}.html"
+			end
+
+			def category_slug(item)
+				slug_for article_category(item)
+			end
+
+			def article_slug(item)
+				slug_for article_title(item)
+			end
+
+			def slug_for(string)
+				string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 			end
 		end
 	end
