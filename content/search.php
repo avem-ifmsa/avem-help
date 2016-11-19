7854453c1d2fdd4c0d6cff9b29280235a45ad33c
Álex Puchades <alex94puchades@gmail.com>
2016-11-19 01:03:48 +0100

    Show query in search box

--- content/search.php
+++ content/search.php
@@ -1,4 +1,5 @@
-<% render '/content.html', search_query: '$_GET["q"]' do %>
+<% query_encoded = '<?php echo htmlspecialchars($_GET["q"]) ?>' %>
+<% render '/content.html', search_query: query_encoded do %>
 	<div class="search-results">
 		<%= gcs_search_results %>
 	</div>
