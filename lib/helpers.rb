afec886c306cd69234e2d5c69ca57ee373ca2260
Álex Puchades <alex94puchades@gmail.com>
2016-11-21 01:48:13 +0100

    Fix url generation code

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,5 +1,11 @@
 include Nanoc::Helpers::Rendering
 
 def slug_for(string)
-	string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+	string.strip.downcase.gsub(' ', '-')
+	      .gsub('á', 'a').gsub('à', 'a').gsub('ä', 'a')
+	      .gsub('é', 'e').gsub('è', 'e').gsub('ë', 'e')
+	      .gsub('í', 'i').gsub('ì', 'i').gsub('ï', 'i')
+	      .gsub('ó', 'o').gsub('ò', 'o').gsub('ö', 'o')
+	      .gsub('ú', 'u').gsub('ù', 'u').gsub('ü', 'u')
+	      .gsub(/[^\w-]/, '')
 end
