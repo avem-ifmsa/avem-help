73af8f32194b29195c22f7eac8a06b1e3289a248
Álex Puchades <alex94puchades@gmail.com>
2016-11-21 20:48:25 +0100

    Fix handling of capitals in slug generation

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,11 +1,12 @@
 include Nanoc::Helpers::Rendering
 
 def slug_for(string)
-	string.strip.downcase.gsub(' ', '-')
-	      .gsub('á', 'a').gsub('à', 'a').gsub('ä', 'a')
-	      .gsub('é', 'e').gsub('è', 'e').gsub('ë', 'e')
-	      .gsub('í', 'i').gsub('ì', 'i').gsub('ï', 'i')
-	      .gsub('ó', 'o').gsub('ò', 'o').gsub('ö', 'o')
-	      .gsub('ú', 'u').gsub('ù', 'u').gsub('ü', 'u')
+	string.strip.downcase
+	      .gsub(' ', '-')
+	      .gsub(/[áàä]/i, 'a')
+	      .gsub(/[éèë]/i, 'e')
+	      .gsub(/[íìï]/i, 'i')
+	      .gsub(/[óòö]/i, 'o')
+	      .gsub(/[úùü]/i, 'u')
 	      .gsub(/[^\w-]/, '')
 end
